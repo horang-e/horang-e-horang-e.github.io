bf53c18d5df07b78edf27f0f90dbb4412655cc6d
서현 <105104335+horang-e@users.noreply.github.com>
2024-10-10 20:05:19 +0900

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,13 @@
 /** @type {import('next').NextConfig} */
-const isProduction = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
   output: 'export',
   reactStrictMode: true,
-  // basePath 명시
-  basePath: isProduction ? '/portfolio' : '',
   images: {
     unoptimized: true
   },
   trailingSlash: true,
-  assetPrefix: !isProduction ? `/portfolio/` : "",
 };
+
 
 export default nextConfig;
